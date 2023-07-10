955b5cafb1398da930a1df9c050d0d8801364036
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-07-10 20:36:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.signal import butter, filtfilt
+from scipy import interpolate
 
 # if you need to access a file next to the source code, use the variable ROOT
 # for example:
@@ -12,7 +15,8 @@ FOLDER = "GaitData"
 CODE_LIST = [filename.replace("_lf.txt", "") for filename in os.listdir(FOLDER) if filename.endswith("_lf.txt")]
 #COLUMN_NAMES = {'PacketCounter': 0, 'Acc_X': 1, 'Acc_Y': 2, 'Acc_Z': 3, 'FreeAcc_X': 4, 'FreeAcc_Y': 5, 'FreeAcc_Z': 6,
 #                'Gyr_X': 7, 'Gyr_Y': 8, 'Gyr_Z': 9, 'Mag_X': 10, 'Mag_Y': 11, 'Mag_Z': 12}
-COLUMN_NAMES = {'TAX': 0, 'TAY': 1, 'TOX': 2, 'RAV': 3, 'RAZ': 4, 'RRY': 5, 'LAV': 6, 'LAZ': 7, 'LRY': 8}
+
+COLUMN_NAMES = {'TAX': 1, 'TAY': 2, 'TOX': 3, 'RAV': 4, 'RAZ': 5, 'RRY': 6, 'LAV': 7, 'LAZ': 8, 'LRY': 9}
 
 
 def load_metadata(subject, trial):
@@ -33,30 +37,6 @@ def load_metadata(subject, trial):
     with open(fname + ".json") as metadata_file:
         metadata_dict = json.load(metadata_file)
     return metadata_dict
-
-
-def load_signal(subject, trial):
-    """Return the signal associated with the subject-trial pair.
-
-    Parameters
-    ----------
-    subject : int
-        Subject number
-    trial : int
-        Trial number
-
-    Returns
-    -------
-    numpy array
-        Signal
-
-    """
-    code = str(subject) + "-" + str(trial)
-    fname = os.path.join(FOLDER, code)
-    signal_lb = np.loadtxt(fname+"_lb.txt", skiprows=1)
-    signal_lf = np.loadtxt(fname+"_lf.txt", skiprows=1)
-    signal_rf = np.loadtxt(fname+"_rf.txt", skiprows=1)
-    return signal
 
 
 def print_trial_info(metadata_dict):
@@ -93,23 +73,126 @@ def print_trial_info(metadata_dict):
     with open("trial_info.txt", "w") as f:
         print(info_msg.format(**display_dict), file=f)
 
+def load_XSens(filename):
+    
+    signal = pd.read_csv(filename, delimiter="\t", skiprows=1, header=0)
+    t = signal["PacketCounter"]
+    t_0 = t[0]
+    t_fin = t[len(t) - 1]
 
-def dump_plot(signal, metadata_dict, to_plot=["RAV", "RAZ", "RRY"]):
+    time = [i for i in range(int(t_0), int(t_fin) + 1)]
+    time_init_0 = [i / 100 for i in range(len(time))]
+    d = {'PacketCounter': time_init_0}
+
+    colonnes = signal.columns
+
+    for colonne in colonnes[1:]:
+        val = signal[colonne]
+        f = interpolate.interp1d(t, val)
+        y = f(time)
+        d[colonne] = y.tolist()
+
+    signal = pd.DataFrame(data=d)
+
+    return signal
+
+
+def import_XSens(path, start=0, end=200, ordre=8, fc=14):
+    data = load_XSens(path)
+    
+    data["FreeAcc_X"] = data["Acc_X"] - np.mean(data["Acc_X"][start:end])
+    data["FreeAcc_Y"] = data["Acc_Y"] - np.mean(data["Acc_Y"][start:end])
+    data["FreeAcc_Z"] = data["Acc_Z"] - np.mean(data["Acc_Z"][start:end])
+
+    data = filtre_sig(data, "Acc", ordre, fc)
+    data = filtre_sig(data, "FreeAcc", ordre, fc)
+    data = filtre_sig(data, "Gyr", ordre, fc)
+
+    return data
+
+
+def filtre_sig(data, type_sig, ordre, fc):
+    data[type_sig + "_X"] = filtre_passe_bas(data[type_sig + "_X"], ordre, fc)
+    data[type_sig + "_Y"] = filtre_passe_bas(data[type_sig + "_Y"], ordre, fc)
+    data[type_sig + "_Z"] = filtre_passe_bas(data[type_sig + "_Z"], ordre, fc)
+
+    return data
+
+
+def filtre_passe_bas(sig, ordre=8, fc=14, fe=100):
+    # Fréquence d'échantillonnage fe en Hz
+    # Fréquence de nyquist
+    f_nyq = fe / 2.  # Hz
+
+    # Préparation du filtre de Butterworth en passe-bas
+    (b, a) = butter(N=ordre, Wn=(fc / f_nyq), btype='low', analog=False)
+
+    # Application du filtre
+    return filtfilt(b, a, sig)
+
+
+
+def load_signal(subject, trial):
+    """Return the signal associated with the subject-trial pair.
+
+    Parameters
+    ----------
+    subject : int
+        Subject number
+    trial : int
+        Trial number
+
+    Returns
+    -------
+    numpy array
+        Signal
+
+    """
+    code = str(subject) + "-" + str(trial)
+    fname = os.path.join(FOLDER, code)
+    signal_lb = import_XSens(fname+"_lb.txt")
+    signal_lf = import_XSens(fname+"_lf.txt")
+    signal_rf = import_XSens(fname+"_rf.txt")
+    
+    t_max = min(len(signal_lb), len(signal_rf), len(signal_lf))
+    signal_lb = signal_lb[0:t_max]
+    signal_lf = signal_lf[0:t_max]
+    signal_rf = signal_rf[0:t_max]
+    
+    sig = {'Time': signal_lb["PacketCounter"], 'TAX': signal_lb["Acc_X"], 'TAY': signal_lb["Acc_Y"], 'TOX': 2, 
+           'RAV': np.sqrt(signal_rf["FreeAcc_X"]**2 + signal_rf["FreeAcc_Y"]**2 + signal_rf["FreeAcc_Z"]**2), 
+           'RAZ': signal_rf["FreeAcc_Z"], 'RRY': signal_rf["Gyr_Y"], 
+           'LAV': np.sqrt(signal_lf["FreeAcc_X"]**2 + signal_lf["FreeAcc_Y"]**2 + signal_lf["FreeAcc_Z"]**2), 
+           'LAZ': signal_lf["FreeAcc_Z"], 'LRY': signal_lf["Gyr_Y"]}
+    
+    signal = pd.DataFrame(sig)
+    
+    return signal
+
+
+def dump_plot(signal, metadata_dict, to_plot=['TAX', 'TAY', 'TOX', 'RAV', 'RAZ', 'RRY', 'LAV', 'LAZ', 'LRY']):
 
     n_samples, _ = signal.shape
     tt = np.arange(n_samples) / 100
 
     # get limits
+    acc_tronc = np.take(signal, indices=[COLUMN_NAMES[dim_name]
+                                   for dim_name in to_plot if dim_name[0:2] == "TA"], axis=1)
+    if acc_tronc.size > 0:
+        acc_tronc_ylim = [acc_tronc.min()-0.1, acc_tronc.max()+0.1]
+    
     acc = np.take(signal, indices=[COLUMN_NAMES[dim_name]
                                    for dim_name in to_plot if dim_name[1] == "A"], axis=1)
     if acc.size > 0:
         acc_ylim = [acc.min()-0.1, acc.max()+0.1]
+        
     rot = np.take(signal, indices=[COLUMN_NAMES[dim_name]
                                    for dim_name in to_plot if dim_name[1] == "R"], axis=1)
     if rot.size > 0:
         rot_ylim = [rot.min()-20, rot.max()+20]
 
     for dim_name in to_plot:
+        print(dim_name, COLUMN_NAMES[dim_name])
         fig, ax = plt.subplots(figsize=(10, 4))
         # xlim
         ax.set_xlim(0, n_samples/100)
@@ -117,29 +200,50 @@ def dump_plot(signal, metadata_dict, to_plot=["RAV", "RAZ", "RRY"]):
         dim = COLUMN_NAMES[dim_name]
         ax.plot(tt, signal[:, dim])
         # ylim
-        if dim_name[1] == "A":
-            ax.set_ylim(acc_ylim)
-        elif dim_name[1] == "R":
-            ax.set_ylim(rot_ylim)
+        if dim_name[0] in ["R", "L"]:
+            if dim_name[1] == "A":
+                ax.set_ylim(acc_ylim)
+            elif dim_name[1] == "R":
+                ax.set_ylim(rot_ylim)
+        elif dim_name[0:2]== "TA":
+            ax.set_ylim(acc_tronc_ylim)
+        
         # number of yticks
         plt.locator_params(axis='y', nbins=6)
         # ylabel
         ylabel = "m/s²" if dim_name[1] == "A" else "deg/s"
-        ax.set_ylabel(ylabel, fontdict={"size": 20})
+        ax.set_ylabel(ylabel, fontdict={"size": 15})
         for z in ax.get_yticklabels() + ax.get_xticklabels():
-            z.set_fontsize(15)
-        # step annotations
-        if dim_name[0] == "R":
-            steps = metadata_dict["RightFootActivity"]
-        elif dim_name[0] == "L":
-            steps = metadata_dict["LeftFootActivity"]
-
+            z.set_fontsize(12)
+        
         ymin, ymax = ax.get_ylim()
-        for start, end in steps:
-            ax.vlines([start/100, end/100], ymin, ymax, linestyles="--", lw=1)
-            ax.fill_between([start/100, end/100], ymin, ymax,
-                            facecolor="green", alpha=0.3)
+        
+        # seg annotations
+        u_start, u_end = metadata_dict["UTurnBoundaries"]
+        ax.vlines([u_start/100, u_end/100], ymin, ymax, color='red', linestyles="--", lw=1, label = 'U-turn Boundaries')
+        ax.fill_between([u_start/100, u_end/100], ymin, ymax,
+                        facecolor="red", alpha=0.2, label = "U-Turn Phase")
+        # step annotations
+        if dim_name[0] in ["R", "L"]:
+            if dim_name[0] == "R":
+                steps = metadata_dict["RightFootEvents"]
+            elif dim_name[0] == "L":
+                steps = metadata_dict["LeftFootEvents"]
+                
+            label_added =False
+            for start, end in steps:
+                if (end < u_start) | (start > u_end):
+                    if not label_added:
+                        ax.vlines([start/100, end/100], ymin, ymax, linestyles="--", lw=1, label = "Gait Events")
+                        r = ax.fill_between([start/100, end/100], ymin, ymax,
+                                        facecolor="green", alpha=0.3, label = "Swing Phases")
+                        label_added =True
+                    else:
+                        ax.vlines([start/100, end/100], ymin, ymax, linestyles="--", lw=1)
+                        r = ax.fill_between([start/100, end/100], ymin, ymax,
+                                        facecolor="green", alpha=0.3)
         fig.tight_layout()
+        fig.legend(bbox_to_anchor=(1, 0.72, 0, 0.5))
         plt.savefig(dim_name + ".svg", dpi=300,
                     transparent=True, bbox_inches='tight')
 
@@ -157,7 +261,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     subject, trial = args.subject, args.trial
-    to_plot = ["TAX", "TAY", "TOX", "RAV", "RAZ", "RRY", "LAV", "LAZ", "LRY"]
+    to_plot = ['TAX', 'TAY', 'TOX', 'RAV', 'RAZ', 'RRY', 'LAV', 'LAZ', 'LRY']
 
     # check if the code exists.
     code = str(subject) + "-" + str(trial)
