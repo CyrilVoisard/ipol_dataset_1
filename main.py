88cc981fb74b4c9b648bac2d4559b6a2076f8a0a
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-08 12:04:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -100,99 +100,13 @@ def load_XSens(filename):
     
     signal = pd.read_csv(filename, delimiter="\t", skiprows=1, header=0)
     signal["PacketCounter"] = (signal["PacketCounter"] - signal["PacketCounter"][0])/100  # conversion (sample to seconds)
-    """t = signal["PacketCounter"]
-    t = t[0]
-    t_fin = t[len(t) - 1]
-
-    time = [i for i in range(int(t_0), int(t_fin) + 1)]
-    time_init_0 = [i / 100 for i in range(len(time))]
-    d = {'PacketCounter': time_init_0}
-
-    colonnes = signal.columns
-
-    for colonne in colonnes[1:]:
-        val = signal[colonne]
-        f = interpolate.interp1d(t, val)
-        y = f(time)
-        d[colonne] = y.tolist()
-
-    signal = pd.DataFrame(data=d)"""
-
+   
     # interest signals centered on zero
     signal["FreeAcc_X"] = signal["Acc_X"] - np.mean(signal["Acc_X"])
     signal["FreeAcc_Y"] = signal["Acc_Y"] - np.mean(signal["Acc_Y"])
     signal["FreeAcc_Z"] = signal["Acc_Z"] - np.mean(signal["Acc_Z"])
 
     return signal
-
-
-def import_XSens(path, start=0, end=200, order=8, fc=14):
-    """Import and pre-process the data from a file.
-
-    Arguments:
-        filename {str} -- file path
-        order {int} -- order of the Butterworth low-pass filter
-        fc {int} -- cut-off frequency of the Butterworth low-pass filter
-
-    Returns
-    -------
-    Pandas dataframe
-        data
-    """
-    
-    data = load_XSens(path)
-
-    # interest signals centered on zero
-    data["FreeAcc_X"] = data["Acc_X"] - np.mean(data["Acc_X"])
-    data["FreeAcc_Y"] = data["Acc_Y"] - np.mean(data["Acc_Y"])
-    data["FreeAcc_Z"] = data["Acc_Z"] - np.mean(data["Acc_Z"])
-
-    return data
-
-
-def filter_sig(data, type_sig, order, fc):
-    """Application of Butterworth low-pass filter to a Dataframe
-
-    Arguments:
-        data {dataframe} -- pandas dataframe
-        type_sig {str} -- "Acc", "Gyr" or "Mag"
-        order {int} -- order of the Butterworth low-pass filter
-        fc {int} -- cut-off frequency of the Butterworth low-pass filter
-
-    Returns
-    -------
-    Pandas dataframe
-        data
-    """
-    data[type_sig + "_X"] = low_pass_filter(data[type_sig + "_X"], order, fc)
-    data[type_sig + "_Y"] = low_pass_filter(data[type_sig + "_Y"], order, fc)
-    data[type_sig + "_Z"] = low_pass_filter(data[type_sig + "_Z"], order, fc)
-
-    return data
-
-
-def low_pass_filter(sig, order=8, fc=14, fe=100):
-    """Definition of a Butterworth low-pass filter
-
-    Arguments:
-        sig {dataframe} -- pandas dataframe
-        order {int} -- order of the Butterworth low-pass filter
-        fc {int} -- cut-off frequency of the Butterworth low-pass filter
-        fe {int} -- acquisition frequency for the data
-    Returns
-    -------
-    ndarray
-        filter
-    """
-    
-    f_nyq = fe / 2.  # Hz
-
-    # definition of the Butterworth low-pass filter
-    (b, a) = butter(N=order, Wn=(fc / f_nyq), btype='low', analog=False)
-
-    # application
-    return filtfilt(b, a, sig)
-
 
 
 def load_signal(subject, trial):
